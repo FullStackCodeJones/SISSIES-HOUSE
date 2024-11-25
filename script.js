e8941c1cde0fc2af2468365d64e34e131be84ec7
FullStackCodeJones <fullstackcodejones@gmail.com>
2024-11-25 16:14:37 -0500

    Redoing style on Get INvolved Page

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("form");
 
+  document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent actual form submission
+    alert("Thank you for getting involved! We will reach out to you soon.");
+    this.reset(); // Clears the form inputs
+  });
+
   form.addEventListener("submit", function (event) {
     event.preventDefault();
 
